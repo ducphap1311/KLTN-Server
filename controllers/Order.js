bac67eea03f99b6a2b3181a83afda333e9c577ae
unknown <hophap1311@gmail.com>
2024-11-26 18:33:58 +0700

    init source

---
+++ controllers/Order.js
@@ -0,0 +1,28 @@
+const Order = require('../model/Order')
+const {NotFoundError} = require('../errors')
+const getOrders = async (req, res) => {
+    const createdBy = req.user.id;
+    const orders = await Order.find({createdBy})
+    res.status(200).json({ orders });
+};
+
+const getAllOrders = async (req, res) => {
+    const orders = await Order.find({})
+    res.status(200).json({orders})
+}
+
+const createOrder = async (req, res) => {
+    req.body.createdBy = req.user.id;
+    const order = await Order.create(req.body)
+    res.status(200).json({order});
+};
+
+const getSingleOrder = async (req, res) => {
+    const id = req.params.id
+    const order = await Order.findOne({_id: id})
+    if(!order){
+        throw new NotFoundError(`No order with id ${id}`)
+    }
+    res.status(200).json({order})
+}
+module.exports = { getOrders, getAllOrders, createOrder, getSingleOrder };
---
+++ routes/Product.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const {
+    getProducts,
+    getSingleProduct,
+    // createProduct,
+    // deleteProduct,
+    // updateProduct,
+} = require("../controllers/Product");
+
+router.route("/products").get(getProducts)
+router
+    .route("/products/:id")
+    .get(getSingleProduct)
+    // .delete(deleteProduct)
+    // .put(updateProduct);
+module.exports = router;
