8e4ccbd4fe9f594544c852780b820a34f976a23e
unknown <hophap1311@gmail.com>
2024-11-30 08:55:26 +0700

    update authenticate google user

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,7 +8,15 @@ const authenticateUser = async (req, res, next) => {
         throw new UnauthenticatedError('No valid token provided')
     }
     const token = authHeader.split(' ')[1]
+    
     try {
+        const googleDecoded = jwt.decode(token)
+        if(googleDecoded.sub && googleDecoded.jti) {
+            const {sub, name: username, email} = googleDecoded
+            req.user = {id: `${sub}123`, username, email, token}
+            next()
+            return;
+        }
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
         const {id, username, email} = decoded
         req.user = {id, username, email, token}
